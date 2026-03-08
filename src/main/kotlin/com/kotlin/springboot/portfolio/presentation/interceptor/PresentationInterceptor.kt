68b4fb36d98cf5eee83b4426296bcb11db4f306a
jaeheonYoo325 <yoojh0325@gmail.com>
2026-03-08 14:46:57 +0900

    인터셉터 개발

--- src/main/kotlin/com/kotlin/springboot/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/kotlin/springboot/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,23 @@
 package com.kotlin.springboot.portfolio.presentation.interceptor
 
+import com.kotlin.springboot.portfolio.domain.entity.HttpInterface
+import com.kotlin.springboot.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
