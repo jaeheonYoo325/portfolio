88ced8aa9941999d1632cc221e2cc560f7c7a4c2
jaeheonYoo325 <yoojh0325@gmail.com>
2026-03-15 18:43:44 +0900

    삽입, 수정 API 개발 - 연관관계 없음

--- src/main/kotlin/com/kotlin/springboot/portfolio/admin/context/skill/service/AdminSkillService.kt
+++ src/main/kotlin/com/kotlin/springboot/portfolio/admin/context/skill/service/AdminSkillService.kt
@@ -1,9 +1,13 @@
 package com.kotlin.springboot.portfolio.admin.context.skill.service
 
+import com.kotlin.springboot.portfolio.admin.context.skill.form.SkillForm
 import com.kotlin.springboot.portfolio.admin.data.TableDTO
+import com.kotlin.springboot.portfolio.admin.exception.AdminBadRequestException
+import com.kotlin.springboot.portfolio.domain.constant.SkillType
 import com.kotlin.springboot.portfolio.domain.entity.Skill
 import com.kotlin.springboot.portfolio.domain.repository.SkillRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminSkillService(
@@ -15,5 +19,24 @@ class AdminSkillService(
         val entities = skillRepository.findAll()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: SkillForm) {
+
+        val skillType = SkillType.valueOf(form.type)
+        skillRepository.findByNameIgnoreCaseAndType(form.name, skillType)
+            .ifPresent { throw AdminBadRequestException("중복된 데이터입니다.") }
+
+        val skill = form.toEntity()
+
+        skillRepository.save(skill)
+    }
+
+    @Transactional
+    fun update(id: Long, form: SkillForm) {
+        val skill = form.toEntity(id)
+
+        skillRepository.save(skill)
     }
 }
