e57565c81d6377d966c7faab20116545dd7af1cc
jaeheonYoo325 <yoojh0325@gmail.com>
2026-03-15 19:23:20 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/kotlin/springboot/portfolio/admin/context/project/service/AdminProjectService.kt
+++ src/main/kotlin/com/kotlin/springboot/portfolio/admin/context/project/service/AdminProjectService.kt
@@ -1,30 +1,80 @@
 package com.kotlin.springboot.portfolio.admin.context.project.service
 
+import com.kotlin.springboot.portfolio.admin.context.project.form.ProjectForm
 import com.kotlin.springboot.portfolio.admin.data.TableDTO
 import com.kotlin.springboot.portfolio.admin.exception.AdminBadRequestException
 import com.kotlin.springboot.portfolio.domain.entity.ExperienceDetail
 import com.kotlin.springboot.portfolio.domain.entity.Project
 import com.kotlin.springboot.portfolio.domain.repository.ProjectRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminProjectService(
     private val projectRepository: ProjectRepository
 ) {
 
-    fun getExperienceTable(): TableDTO {
+    fun getProjectTable(): TableDTO {
         val classInfo = Project::class
         val entities = projectRepository.findAll()
 
         return TableDTO.from(classInfo, entities, "details", "skills")
     }
 
-    fun getExperienceDetailTable(id: Long?): TableDTO {
+    fun getProjectDetailTable(id: Long?): TableDTO {
         val classInfo = ExperienceDetail::class
         val entities = if (id != null) projectRepository.findById(id)
             .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니다.") }
             .details else emptyList()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: ProjectForm) {
+
+        val projectDetails = form.details
+            ?.map { detail -> detail.toEntity() }
+            ?.toMutableList()
+
+        val project = form.toEntity()
+        project.addDetails(projectDetails)
+
+        projectRepository.save(project)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ProjectForm) {
+
+        val project = projectRepository.findById(id)
+            .orElseThrow {
+                throw AdminBadRequestException(
+                    "ID ${id}에 해당하는 데이터를 찾을 수 없습니다."
+                )
+            }
+
+        project.update(
+            name = form.name,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = project.details.map { it.id to it }.toMap()
+        form.details?.forEach {
+            val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    url = it.url,
+                    isActive = it.isActive
+                )
+            } else {
+                project.details.add(it.toEntity())
+            }
+        }
     }
 }
